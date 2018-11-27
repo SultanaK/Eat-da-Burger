f653e6409258abfbd4404500cc0c2a9b469ef183
SultanaK <kr.shampa@yahoo.com>
2018-11-27 01:34:53 -0600

    update seed file

--- database/seeds.sql
+++ database/seeds.sql
@@ -2,3 +2,5 @@ USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('HamBurger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('MixVegetable burger', false);
