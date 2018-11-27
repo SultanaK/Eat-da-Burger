4183ab3e424285a634722c50af80b83e0d09b680
SultanaK <kr.shampa@yahoo.com>
2018-11-26 19:11:56 -0600

    upadting files

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mOverride = require("method-override");
+var port = process.env.PORT || 3000;
+var app = express();
+
+
+
+app.use(mOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/routes.js");
+
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+app.listen(port);
